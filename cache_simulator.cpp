12a1d01adf4dd9e37e1a417fb2f51887fedc9b72
YoungSeok Joo <robinjoo1015@naver.com>
2019-12-21 05:00:18 +0900

    Address to unsigned int

--- cache_simulator.cpp
+++ cache_simulator.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-#include <string>
-#include <cstdlib>
 #include <fstream>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
 #include <vector>
+#include <set>
+#include <bitset>
 using namespace std;
 
 vector<string> addresses;
@@ -12,9 +15,9 @@ void help()
 {
 	cout << "Wrong argument" << endl;
 	cout << "cache_simulator -s <S> -n <N> -m <M> <input_file_name>" << endl;
-	cout << "	-s <S> : the number of sets in a cache" << endl;
-	cout << "	-n <N> : the number of blocks in a set" << endl;
-	cout << "	-m <M> : the number of words in a block" << endl;
+	cout << "	-s <S> : the number of sets in a cache (power of 2)" << endl;
+	cout << "	-n <N> : the number of blocks in a set (power of 2)" << endl;
+	cout << "	-m <M> : the number of words in a block (power of 2)" << endl;
 }
 
 bool readFile(string file_name)
@@ -60,11 +63,21 @@ int main(int argc, char *argv[])
 		}
 	}
 	
-	if(!readFile(input_file_name)) {
-		cout << "Input File Error" << endl;
+	if(!s%2||!n%2||!m%2) {
+		help();
 		return 3;
 	}
 	
+	if(!readFile(input_file_name)) {
+		cout << "Input File Error" << endl;
+		return 4;
+	}
+	
+	for(int i = 0; i < addresses.size(); i++) {
+		unsigned int ca = strtoul(addresses[i].c_str(), NULL, 0);
+		bitset<32> x(ca);
+		cout << addresses[i] << " : " << ca << " : " << x << endl;
+	}
 	
 	return 0;
 }
